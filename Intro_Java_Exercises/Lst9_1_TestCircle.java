2ae1e86db3fd2fbb087997151ce2912a376006b5
“X-hung” <x.hung.chn@gmail.com>
2018-12-30 21:20:23 +0800

    add new exercise

---
+++ Intro_Java_Exercises/Lst9_1_TestCircle.java
@@ -0,0 +1,38 @@
+public class Lst9_1_TestCircle {
+    public static void main(String[] args) {
+        Circle circle1 = new Circle();
+        System.out.println("C1: ");
+        System.out.println("Radius: " + circle1.radius + " | Perimeter: " + circle1.getPerimeter() + " | Area: " + circle1.getArea());
+        
+        Circle circle2 = new Circle(25);
+        System.out.println("C2: ");
+        System.out.println("Radius: " + circle2.radius + " | Perimeter: " + circle2.getPerimeter() + " | Area: " + circle2.getArea());
+        
+        Circle circle3 = new Circle(125);
+        System.out.println("C3: ");
+        System.out.println("Radius: " + circle3.radius + " | Perimeter: " + circle3.getPerimeter() + " | Area: " + circle3.getArea());
+    }
+}
+    class Circle {
+        double radius;
+
+        Circle() {
+            radius = 1;
+        }
+
+        Circle(double newRadius) {
+            radius = newRadius;
+        }
+
+        double getArea() {
+            return radius * radius * Math.PI;
+        }
+
+        double getPerimeter() {
+            return 2 * radius * Math.PI;
+        }
+
+        void setRadius(double newRadius) {
+            radius = newRadius;
+        }
+    }
