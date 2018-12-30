9dab6a582c3fee7ebb20b8f79c4e199b3a1ae2f6
“X-hung” <x.hung.chn@gmail.com>
2018-12-30 22:01:11 +0800

    more exercises

--- Intro_Java_Exercises/Lst9_2_AlternativeCircle.java
+++ Intro_Java_Exercises/Lst9_2_AlternativeCircle.java
@@ -1,31 +1,31 @@
 /**
- * Lst9_1_TestCircle
- * 面向对象编程中类的编写的第1种形式：与主类成平行结构，只不过不能加访问限制符。
- * 比较推荐
+ * Lst9_2_AlternativeCircle
+ * 面向对象编程中类的编写的第1种形式：与main放在同一个主类中，通过main调用直接测试
+ * 不推荐
  */
-public class Lst9_1_TestCircle {
+public class Lst9_2_AlternativeCircle {
+
     public static void main(String[] args) {
-        Circle circle1 = new Circle();
+        Lst9_2_AlternativeCircle circle1 = new Lst9_2_AlternativeCircle();
         System.out.println("C1: ");
         System.out.println("Radius: " + circle1.radius + " | Perimeter: " + circle1.getPerimeter() + " | Area: " + circle1.getArea());
         
-        Circle circle2 = new Circle(25);
+        Lst9_2_AlternativeCircle circle2 = new Lst9_2_AlternativeCircle(25);
         System.out.println("C2: ");
         System.out.println("Radius: " + circle2.radius + " | Perimeter: " + circle2.getPerimeter() + " | Area: " + circle2.getArea());
         
-        Circle circle3 = new Circle(125);
+        Lst9_2_AlternativeCircle circle3 = new Lst9_2_AlternativeCircle(125);
         System.out.println("C3: ");
-        System.out.println("Radius: " + circle3.radius + " | Perimeter: " + circle3.getPerimeter() + " | Area: " + circle3.getArea());
+        System.out.println("Radius: " + circle3.radius + " | Perimeter: " + circle3.getPerimeter() + " | Area: " + circle3.getArea());   
     }
-}
-class Circle {
+
     double radius;
 
-    Circle() {
+    Lst9_2_AlternativeCircle() {
         radius = 1;
     }
 
-    Circle(double newRadius) {
+    Lst9_2_AlternativeCircle(double newRadius) {
         radius = newRadius;
     }
 
